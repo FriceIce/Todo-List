a1cd7d5c45a1098fc1f35232e1e7ac16b318fa90
Isak Tilahun <you@example.com>
2024-02-22 17:33:39 +0100

    README med resonemang kring applikationens-programkod.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Todo-List
-Todo-project, Chasacademy. 
+Todo-list, Chasacademy. 
+
+# Resonemang kring min kod.
+
+**Styrkor:**
+Jag har påbörjat utvecklingen av en Todo-list applikation med hjälp av TypeScript och strukturerat koden med moduler som exporteras och importeras enligt ES6-standarden. En tydlig styrka i koden är den statiska typningen som TypeScript erbjuder. Det leder till en kod som är lättare att läsa och ger kontinuerligt stöd med metoder från objektens prototypkedja, eftersom programmet alltid vet vilket värde en specifik programkod ska innehålla vid exekvering. Dessutom möjliggör TypeScript felhantering innan exekvering, vilket har varit av stor nytta för mig.
+
+Användningen av moduler i projektet har resulterat i en strukturerad mapp- och filstruktur. Det blir enklare att felsöka och navigera bland applikationens filer eftersom modulerna har namngivits efter den specifika uppgift de utför. Detta har också bidragit till minskad kodmängd i main.ts (huvudfilen), vilken nu endast innehåller eventhanterare och import av moduler.
+
+**Brister:**
+Även om jag tidigare nämnde moduler som en av kodens styrkor, så finns det en nackdel med min erfarenhet av dem. En nackdel är min osäkerhet kring hur man strukturerar modulerna på det mest effektiva sättet. Frågor som om applikationens mapp-/filstruktur följer bästa praxis, om modulerna bör få mer beskrivande filnamn, eller om det finns för få eller för många moduler, är frågor som jag inte har svar på. Denna osäkerhet känner jag är en brist i applikationens programkod och är något jag behöver kolla in på lite närmare. 
--- Todo-typescript/src/main.ts
+++ Todo-typescript/src/main.ts
@@ -7,23 +7,20 @@ import addTask from './modules/modules-for-addEventListeners.ts/add-task';
 import filterSystem from "./modules/modules-for-addEventListeners.ts/filter-Task";
 import removeOrEditTask from './modules/modules-for-addEventListeners.ts/remove-or-edit-task';
 
-// ---------------------TOGGLE TASK AS FINNISHED OR NOT FINISHED-----------------------------
-ulEl.addEventListener('click', (el: MouseEvent) => toogleStatus(el));
-
-// ----------------------SIDE MENU---------------------
-menuEl.addEventListener('click', sideMenu);
-
-// ---------------------ADD TASKS----------------------
-formEl.addEventListener('submit', (el: SubmitEvent) => addTask(el, ulEl));
-
-// --------------------FILTER TASKS---------------------
-categories.addEventListener('click', (el: MouseEvent) => filterSystem(el, ulEl));
-
-
-// --------------------REMOVE TASK or EDIT TASK---------------------------
-ULForTask.addEventListener('click', (el) => removeOrEditTask(el));
-
-
 // ----------------Render Tasks-----------------------
 renderHTML(ulEl); 
-// --------------------------------------------------
+
+// -----------------TOGGLE TASK AS FINNISHED OR NOT FINISHED--------------------
+ulEl.addEventListener('click', (el: MouseEvent) => toogleStatus(el));
+
+// ----------------------SIDE MENU----------------------------------------------
+menuEl.addEventListener('click', sideMenu);
+
+// ---------------------ADD TASKS-----------------------------------------------
+formEl.addEventListener('submit', (el: SubmitEvent) => addTask(el, ulEl));
+
+// --------------------FILTER TASKS---------------------------------------------
+categories.addEventListener('click', (el: MouseEvent) => filterSystem(el, ulEl));
+
+// --------------------REMOVE TASK or EDIT TASK---------------------------------
+ULForTask.addEventListener('click', (el) => removeOrEditTask(el));
